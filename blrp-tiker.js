6880f6ba689018cb5a32cb0ab1ad38fab719b3b9
ZvezdocheTag <vyastrem@gmail.com>
2017-12-07 00:19:06 +0300

    next

---
+++ blrp-tiker.js
@@ -0,0 +1,24 @@
+// builtin-rate-limiting-rest-poller.js
+const ccxt    = require ('ccxt')
+const log     = require ('ololog')
+const ansi    = require ('ansicolor').nice
+const asTable = require ('as-table')
+
+const exchange = new ccxt.gdax ({ enableRateLimit: true })
+const repeat   = 100;
+
+async function test (symbol) {
+
+    for (let i = 0; i < repeat; i++) {
+        let ticker = await exchange.fetchTicker (symbol)
+        log (exchange.id.green, exchange.iso8601 (exchange.milliseconds ()), ticker['datetime'], symbol.green, ticker['last'])
+    }
+}
+
+const concurrent = [
+    test ('BTC/USD'),
+    test ('ETH/BTC'),
+    test ('ETH/USD')
+]
+
+Promise.all (concurrent)
